d06359bb781c77a5558df4c9558758781b833218
Coffeeteer <tgrieshiemer@gmail.com>
2016-06-13 18:42:00 -0400

    initial commit

---
+++ app.js
@@ -0,0 +1,4 @@
+//1 .Train Name, Destination, First Train Time (military time), and frequency (minutes).
+//2. The app should have code able to use this basic information and self-determine when the next train is to arrive relative to the current time.
+//3. This app must be such that multiple users from -- many different machines can all view the same times simultaneously.
+ 
